02973be7e48a7dbad768b4ceb76884f7b82f45c5
Ron Ang <ron@cpdone.com>
2015-05-07 10:43:07 +0800

    Specflow test + RunSpecFlow.bat

---
+++ RunSpecFlow.bat
@@ -0,0 +1,11 @@
+"C:\Program Files (x86)\NUnit 2.6.4\bin\nunit-console.exe" /labels /out=TestResult.txt /xml=TestResult.xml /framework=net-4.5 Bowling.Specflow\bin\Debug\Bowling.Specflow.dll
+ 
+"C:\Users\rang\Documents\workspace\teamcitytest\packages\SpecFlow.1.9.0\tools\specflow.exe" nunitexecutionreport Bowling.Specflow\Bowling.SpecFlow.csproj
+ 
+IF NOT EXIST TestResult.xml GOTO FAIL
+IF NOT EXIST TestResult.html GOTO FAIL
+EXIT
+ 
+:FAIL
+echo ##teamcity[buildStatus status='FAILURE']
+EXIT /B 1
