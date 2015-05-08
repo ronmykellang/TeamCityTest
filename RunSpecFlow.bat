12a0d7d0f9105b23b64c66ae922026ea5c44dbef
Ron Ang <ron@cpdone.com>
2015-05-08 10:55:52 +0800

    Test

--- RunSpecFlow.bat
+++ RunSpecFlow.bat
@@ -1,6 +1,6 @@
 "C:\Program Files (x86)\NUnit 2.6.4\bin\nunit-console.exe" /labels /out=TestResult.txt /xml=TestResult.xml /framework=net-4.5 Bowling.Specflow\bin\Debug\Bowling.Specflow.dll
  
-"C:\Users\rang\Documents\workspace\teamcitytest\packages\SpecFlow.1.9.0\tools\specflowa.exe" nunitexecutionreport Bowling.Specflow\Bowling.SpecFlow.csproj
+packages\SpecFlow.1.9.0\tools\specflow.exe nunitexecutionreport Bowling.Specflow\Bowling.SpecFlow.csproj
  
 IF NOT EXIST TestResult.xml GOTO FAIL
 IF NOT EXIST TestResult.html GOTO FAIL
